e21bc43f7f444400223469c09413ad7bd8ba0ffa
Jason Prasad <jasongprasad@gmail.com>
2018-09-29 16:04:15 -0400

    My first post

---
+++ Source/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReddiX
+//
+//  Created by Jason Prasad on 9/29/18.
+//  Copyright © 2018 Jason Prasad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
