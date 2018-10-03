fc9ab774db8f26fbf9d698a12b2b3d4414221c90
Jason Prasad <jasongprasad@gmail.com>
2018-10-02 20:52:18 -0400

    updating ExpectAsyncAction to assert file an line

--- UNITests/ExpectAsyncAction.swift
+++ UNITests/ExpectAsyncAction.swift
@@ -16,32 +16,33 @@ class ExpectAsyncAction<State: StateType>: XCTestExpectation {
     private var dispatch: DispatchFunction {
         return { action in
             let matcher = self.expectedDispatches.remove(at: 0)
-            assert(matcher(action))
+            matcher(action)
             if self.expectedDispatches.count == 0 {
                 self.fulfill()
             }
         }
     }
-    private typealias DispatchMatcher = (Action) -> Bool
+    private typealias DispatchMatcher = (Action) -> Void
     private var expectedDispatches = [DispatchMatcher]()
+    private var expectedStates = [State]()
     private var getState: () -> State? {
         return {
             return self.expectedStates.remove(at: 0)
         }
     }
-    private var expectedStates = [State]()
     
     init(_ action: AsyncAction, description: String? = nil) {
         self.action = action
         super.init(description: description ?? "ExpectAsyncAction: \(action)")
     }
     
-    func dispatches<A: Action & Equatable>(_ action: A) -> Self {
-        func matcher(dispatched: Action) -> Bool {
+    func dispatches<A: Action & Equatable>(_ expected: A, file: StaticString = #file, line: UInt = #line) -> Self {
+        func matcher(dispatched: Action) {
             guard let dispatched = dispatched as? A else {
-                return false
+                XCTFail("dispatch fails protocol assertion: \(dispatch)", file: file, line: line)
+                return
             }
-            return dispatched == action
+            XCTAssert(dispatched == expected, "dispatched does not equal expected: \(dispatch) \(expected)", file: file, line: line)
         }
         expectedDispatches.append(matcher)
         return self
